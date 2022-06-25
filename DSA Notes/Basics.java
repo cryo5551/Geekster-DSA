88140fbce68737370b9561fed1f3d66aed391eb6
cryo5551 <96884049+cryo5551@users.noreply.github.com>
2022-06-25 14:40:21 +0530

    Create Basics.java

---
+++ DSA Notes/Basics.java
@@ -0,0 +1,159 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+// arthmatic oprators
+        //  float a =54;
+        // int b =20; 
+        // System.out.println((a/b)+b);
+        // Scanner scn = new Scanner(System.in);
+        // int w = scn.nextInt();
+        // System.out.println(w/b);
+        // int mac = 34;
+        // int guts = mac++;
+        // System.out.println(guts);
+        // int cas = ++mac;
+        // System.out.println(cas);
+        // int uno = --mac;
+        // System.out.println(uno);
+        
+// string as input
+        // Scanner scn = new Scanner (System.in);
+        // // Scanner scn = new Scanner (System.in);
+        // String str = scn.nextLine();
+        // //nextLine scans the whole line 
+        // //next will take only first word
+        // String str2 = scn.next();
+        // System.out.println(str+" "+str2);
+        // // System.out.println(str2);
+                
+        //     System.out.println("Enter the Width And length");
+        // Scanner scn = new Scanner(System.in);
+        
+//         int width = scn.nextInt();
+//         int length = scn.nextInt();
+        
+//         System.out.println("width = "+ width);
+//         System.out.println("length = "+ length);
+        
+    //logical oprators
+    //if the student is 18+ and have o grade
+//               System.out.println("Enter the age and marks");
+//         Scanner scn = new Scanner(System.in);
+//         int age = scn.nextInt();
+//         int marks = scn.nextInt();
+        
+//         System.out.println("Age = "+ age);
+//         System.out.println("Marks = "+ marks);
+        
+//         int o = 90;
+        // int a = 80;
+        // int b = 60;
+        // int c = 45;
+        // int d = 33;
+        
+        
+        // System.out.println(age >= 18 && marks >=o);
+        // System.out.println(age >= 18 || marks >=o);
+        
+    // student is male and getting o grade
+        
+//         System.out.println("Enter the age and marks");
+//         Scanner scn = new Scanner(System.in);
+//         char gender = scn.next().charAt(0);
+//         int marks = scn.nextInt();
+        
+//         System.out.println("Gender = "+ gender);
+//         System.out.println("Marks = "+ marks);
+        
+//         System.out.println(gender == 'M' && marks >= 90);
+//         System.out.println(gender != 'M' || marks >= 90);
+//         int x= 65;
+//         int count= 0;
+//          for(count = 0 ; x!=0 ; count++) {
+//                     x= x/10;
+//                 }
+//                 System.out.println(count);
+//          if(count < 3){
+//                     for(int n=count; n<3; n++){
+//                         int k=0;
+                        
+//                         System.out.println(k + x);
+//                     }
+//                 } 
+
+       
+//         System.out.println("The Area of the Ractangle is = "+ width*length);
+//         System.out.println("The Perimeter of the Ractangle is = " + 2*(width+length));
+//         System.out.println("hii i'm "+ (19+1));
+        
+        // System.out.println(width < length);
+        // System.out.println(width > length);
+        // System.out.println(width == length);
+        // System.out.println(width != length);
+        // System.out.println(width <= length);
+        // System.out.println(width >= length);
+
+        
+// if/else
+        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        // char gender = scan.next().charAt(0);
+        
+//                 System.out.println(gender);
+//         if (gender == 'M')
+//         {
+//             System.out.println("He/His/Him");
+
+//         }
+        
+//         else if (gender == 'F')
+//         {
+//             System.out.println("She/Her");
+
+//         }
+        
+//         else 
+//         {
+//             System.out.println("Other");
+//         }
+        System.out.println("Note:- Please enter G for general, T for ST, C for SC, and E for EWS");
+            
+        char category = scan.next().charAt(0);
+        int marks = scan.nextInt();
+         
+              System.out.println(category);
+              System.out.println(marks);
+        
+        if (marks>=50)
+        {
+                System.out.println("cutoff cleared");  
+        }
+        
+        else if(marks>=33)
+        {
+            if(category == 'T')
+            {
+                System.out.println("cutoff cleared");  
+            }
+            
+            else if (category == 'C')
+            {
+                System.out.println("cutoff cleared");  
+            }
+            
+            else
+            {
+                System.out.println("cutoff not cleared");
+            }
+        }
+        
+         else
+            {
+                System.out.println("cutoff not cleared");
+            }   
+    }
+}
